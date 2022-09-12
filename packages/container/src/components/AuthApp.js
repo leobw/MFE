d3f016a92733bd0e0e5d063194b7014d89bff3cf
Leonardo <leonardo@bannerwise.io>
2022-09-12 22:41:32 +0200

    SignIn and SignOut in Place'

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
 	const ref = useRef(null);
 	const history = useHistory();
 
@@ -12,10 +12,10 @@ export default () => {
 			initialPath: history.location.pathname,
 			onNavigate:	({ pathname: nextPathName }) => {
 				const { pathname } = history.location;
-
 				if(pathname !== nextPathName)
 					history.push(nextPathName);
-			}
+			},
+			onSignIn
 		});
 
 		history.listen(onParentNavigate);
